d4068e71e4a728acac76ea019857f09aa52a760d
KANDA Daisuke <dai1975@gmail.com>
2017-05-31 03:36:02 +0900

    impl deserialize str

--- src/serialize2/deserializer.rs
+++ src/serialize2/deserializer.rs
@@ -4,35 +4,34 @@ use super::{ReadStream, DeserializeError};
 
 pub struct Deserializer<R:ReadStream> {
    r: R,
+   read_bytes: usize,
 }
 
 impl <R:ReadStream> Deserializer<R> {
    pub fn new(r:R) -> Self {
-      Self { r:r }
+      Self { r:r, read_bytes:0 }
    }
    pub fn into_inner(self) -> R {
       self.r
    }
 
-   fn deserialize_varint(&mut self, v:&mut u64) -> Result<usize, ::std::io::Error> {
+   fn deserialize_varint(&mut self) -> Result<usize, ::std::io::Error> {
       let mut x:u8 = 0;
-      try!(self.r.read_u8(&mut x));
+      self.read_bytes += try!(self.r.read_u8(&mut x));
       if x < 253 {
-         *v = x as u64;
-         Ok(1)
+         Ok(x as usize)
       } else if x == 253 {
          let mut y:u16 = 0;
-         try!(self.r.read_u16le(&mut y));
-         *v = y as u64;
-         Ok(3)
+         self.read_bytes += try!(self.r.read_u16le(&mut y));
+         Ok(y as usize)
       } else if x == 254 {
          let mut y:u32 = 0;
-         try!(self.r.read_u32le(&mut y));
-         *v = y as u64;
-         Ok(5)
+         self.read_bytes += try!(self.r.read_u32le(&mut y));
+         Ok(y as usize)
       } else {
-         try!(self.r.read_u64le(v));
-         Ok(9)
+         let mut y:u64 = 0;
+         self.read_bytes += try!(self.r.read_u64le(&mut y));
+         Ok(y as usize)
       }
    }
 }
@@ -47,7 +46,7 @@ impl <'a, R:ReadStream> serde::de::Deserializer for &'a mut Deserializer<R> {
    /// Hint that the `Deserialize` type is expecting a `bool` value.
    fn deserialize_bool<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: serde::de::Visitor {
       let mut x:u8 = 0;
-      try!(self.r.read_u8(&mut x));
+      self.read_bytes += try!(self.r.read_u8(&mut x));
       visitor.visit_bool(x == 1)
       // std::error::Error から DeserializeError への変換は不要?
       //visitor.visit_bool(x == 1).map_err(|e:std::error::Error|{DeserializeError::custom(e)})
@@ -55,49 +54,49 @@ impl <'a, R:ReadStream> serde::de::Deserializer for &'a mut Deserializer<R> {
    
    fn deserialize_u8<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: serde::de::Visitor {
       let mut x:u8 = 0;
-      try!(self.r.read_u8(&mut x));
+      self.read_bytes += try!(self.r.read_u8(&mut x));
       visitor.visit_u8(x)
    }
 
    fn deserialize_u16<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: serde::de::Visitor {
       let mut x:u16 = 0;
-      try!(self.r.read_u16le(&mut x));
+      self.read_bytes += try!(self.r.read_u16le(&mut x));
       visitor.visit_u16(x)
    }
        
    fn deserialize_u32<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: serde::de::Visitor {
       let mut x:u32 = 0;
-      try!(self.r.read_u32le(&mut x));
+      self.read_bytes += try!(self.r.read_u32le(&mut x));
       visitor.visit_u32(x)
    }
       
    fn deserialize_u64<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: serde::de::Visitor {
       let mut x:u64 = 0;
-      try!(self.r.read_u64le(&mut x));
+      self.read_bytes += try!(self.r.read_u64le(&mut x));
       visitor.visit_u64(x)
    }
       
    fn deserialize_i8<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: serde::de::Visitor {
       let mut x:i8 = 0;
-      try!(self.r.read_i8(&mut x));
+      self.read_bytes += try!(self.r.read_i8(&mut x));
       visitor.visit_i8(x)
    }
       
    fn deserialize_i16<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: serde::de::Visitor {
       let mut x:i16 = 0;
-      try!(self.r.read_i16le(&mut x));
+      self.read_bytes += try!(self.r.read_i16le(&mut x));
       visitor.visit_i16(x)
    }
       
    fn deserialize_i32<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: serde::de::Visitor {
       let mut x:i32 = 0;
-      try!(self.r.read_i32le(&mut x));
+      self.read_bytes += try!(self.r.read_i32le(&mut x));
       visitor.visit_i32(x)
    }
 
    fn deserialize_i64<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: serde::de::Visitor {
       let mut x:i64 = 0;
-      try!(self.r.read_i64le(&mut x));
+      self.read_bytes += try!(self.r.read_i64le(&mut x));
       visitor.visit_i64(x)
    }
        
@@ -112,28 +111,34 @@ impl <'a, R:ReadStream> serde::de::Deserializer for &'a mut Deserializer<R> {
    }
 
    fn deserialize_str<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: serde::de::Visitor {
-      deserialize_error!("not implemented")
+      let len = try!(self.deserialize_varint());
+      let mut buf:Vec<u8> = vec![0; len];
+      self.read_bytes += self.r.read_exact(buf.as_mut_slice());
+      let s = try!(str::from_utf8(&buf[0..]));
+      visitor.visit_str(s)
    }
    fn deserialize_string<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: serde::de::Visitor {
-      deserialize_error!("not implemented")
+      let len = try!(self.deserialize_varint());
+      let mut buf:Vec<u8> = vec![0; len];
+      self.read_bytes += self.r.read_exact(buf.as_mut_slice());
+      let s = try!(String::from_utf8(&buf[0..]));
+      visitor.visit_str(s)
    }
       
-    /// Hint that the `Deserialize` type is expecting a byte array and does not
-    /// benefit from taking ownership of buffered data owned by the
-    /// `Deserializer`.
-    ///
-    /// If the `serde::de::Visitor` would benefit from taking ownership of `Vec<u8>` data,
-    /// indicate this to the `Deserializer` by using `deserialize_byte_buf`
-    /// instead.
    fn deserialize_bytes<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: serde::de::Visitor {
-      self.deserialize_
-      deserialize_error!("not implemented")
+      // Copy bytes to continuous buffer because a ReadStream may not have uncontinuous internal buffer.
+      let len = try!(self.deserialize_varint());
+      let mut buf:Vec<u8> = vec![0; len];
+      self.read_bytes += self.r.read_exact(buf.as_mut_slice());
+      visitor.visit_bytes(buf.as_slice())
    }
    fn deserialize_byte_buf<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: serde::de::Visitor {
-      deserialize_error!("not implemented")
+      let len = try!(self.deserialize_varint());
+      let mut buf:Vec<u8> = vec![0; len];
+      self.read_bytes += self.r.read_exact(buf.as_mut_slice());
+      visitor.visit_byte_buf(buf)
    }
       
-
     /// Hint that the `Deserialize` type is expecting an optional value.
     ///
     /// This allows deserializers that encode an optional value as a nullable
@@ -143,7 +148,6 @@ impl <'a, R:ReadStream> serde::de::Deserializer for &'a mut Deserializer<R> {
       deserialize_error!("not implemented")
    }
       
-
     /// Hint that the `Deserialize` type is expecting a unit value.
    fn deserialize_unit<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: serde::de::Visitor {
       deserialize_error!("not implemented")
@@ -175,8 +179,14 @@ impl <'a, R:ReadStream> serde::de::Deserializer for &'a mut Deserializer<R> {
        
 
     /// Hint that the `Deserialize` type is expecting a sequence of values.
-   fn deserialize_seq<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: serde::de::Visitor {
-      deserialize_error!("not implemented")
+   fn deserialize_seq<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: serde::de::Visitor
+   {
+      let len = try!(self.deserialize_varint());
+      let v = try!(visitor.visit_seq(DeserializerSeqVisitor {
+         de:self,
+         len:Some(len as usize),
+      }));
+      Ok(v)
    }
        
 
@@ -188,7 +198,11 @@ impl <'a, R:ReadStream> serde::de::Deserializer for &'a mut Deserializer<R> {
                                      -> Result<V::Value, Self::Error>
       where V: serde::de::Visitor
    {
-      deserialize_error!("not implemented")
+       let v = try!(visitor.visit_seq(DeserializerSeqVisitor {
+          de:self,
+          len:Some(len)
+       }));
+       Ok(v)
    }
        
 
@@ -265,4 +279,27 @@ impl <'a, R:ReadStream> serde::de::Deserializer for &'a mut Deserializer<R> {
       deserialize_error!("not implemented")
    }
        
+}
+
+
+struct DeserializerSeqVisitor<'a> {
+   de: &'a mut Deserializer<I>,
+   len: Option<usize>,
+}
+
+impl<'a> serde::de::SeqVisitor for DeserializerSeqVisitor<'a>
+{
+   type Error = Error;
+
+   fn visit_seed<T>(&mut self, seed: T) -> Result<Option<T::Value>, Error>
+      where T: serde::de::DeserializeSeed
+   {
+      self.len = self.len.map(|len| len.saturating_sub(1));
+      seed.deserialize(&mut *self.de).map(Some)
+   }
+
+   fn size_hint(&self) -> (usize, Option<usize>) {
+      let len = self.len.unwrap_or(0);
+      (len, self.len)
+   }
 }
