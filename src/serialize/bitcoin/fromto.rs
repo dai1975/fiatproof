516807d0dc5f92c768b9797b9877b13fbc50d5f2
Daisuke Kanda <dai1975@gmail.com>
2017-10-14 19:22:52 +0900

    refactoring

--- src/serialize/bitcoin/fromto.rs
+++ src/serialize/bitcoin/fromto.rs
@@ -1,17 +1,19 @@
 use super::Medium;
 
-fn parse_opt(opt:&str) -> Result<(bool, Medium), ::ParseError> {
-   let r = opt.split(',').fold(Ok((false,"".to_string())), |acc,s| {
+const FLAG_UNSIZED:u32      = 1u32 << 0;
+
+fn parse_opt(opt:&str) -> Result<(u32, Medium), ::ParseError> {
+   let r = opt.split(',').fold(Ok((0, "".to_string())), |acc,s| {
       match (acc,s) {
-         (Err(e), _)            => Err(e),
-         (Ok((_,rest)), "unsized") => Ok((true,rest)),
-         (Ok((b,rest)), _)         => Ok((b, rest + s)),
+         (Err(e), _)               => Err(e),
+         (Ok((f,rest)), "unsized") => Ok((f|FLAG_UNSIZED, rest)),
+         (Ok((f,rest)), _)         => Ok((f, rest + s)),
       }
    });
    match r {
       Err(e) => Err(e),
-      Ok((b,rest)) => match Medium::new(rest.as_str()) {
-         Ok(m)  => Ok((b,m)),
+      Ok((flags,rest)) => match Medium::new(rest.as_str()) {
+         Ok(m)  => Ok((flags,m)),
          Err(e) => Err(e),
       },
    }
@@ -22,19 +24,19 @@ use super::{Encoder, Encodee, Decoder, Decodee};
 
 impl <E> ToOctets<Encodee> for E where E:Encodee {
    fn to_octets(&self, opt:&str) -> ::Result<Vec<u8>> {
-      let (b,m) = try!(parse_opt(opt));
+      let (flags, medium) = try!(parse_opt(opt));
       let mut v = {
          let mut w = VecWriteStream::default();
          {
-            let mut e = Encoder::new(&mut w, &m);
+            let mut e = Encoder::new(&mut w, &medium);
             let _ = try!(self.encode(&mut e));
          }
          w.into_inner()
       };
-      if b {
+      if flags & FLAG_UNSIZED != 0 {
          let skipsize = {
             let mut r = SliceReadStream::new(v.as_slice());
-            let mut d = Decoder::new(&mut r, &m);
+            let mut d = Decoder::new(&mut r, &medium);
             let mut size = 0u64;
             let x = try!(d.decode_var_int(&mut size));
             let size = size as usize;
@@ -52,20 +54,21 @@ impl <E> ToOctets<Encodee> for E where E:Encodee {
 
 impl <T> OutofOctets<Decodee> for T where T:Decodee {
    fn outof_octets<S:AsRef<[u8]>>(&mut self, s:S, opt:&str) -> ::Result<usize> {
-      let (b,m) = try!(parse_opt(opt));
-      if b {
+      let (flags, medium) = try!(parse_opt(opt));
+      
+      if flags & FLAG_UNSIZED != 0 {
          let mut w = VecWriteStream::default();
          {
-            let mut e = Encoder::new(&mut w, &m);
+            let mut e = Encoder::new(&mut w, &medium);
             let _ = try!(e.encode_var_octets(s.as_ref(), ::std::usize::MAX));
          }
          let v = w.into_inner();
          let mut r = SliceReadStream::new(v.as_slice());
-         let mut d = Decoder::new(&mut r, &m);
+         let mut d = Decoder::new(&mut r, &medium);
          self.decode(&mut d)
       } else {
          let mut r = SliceReadStream::new(s.as_ref());
-         let mut d = Decoder::new(&mut r, &m);
+         let mut d = Decoder::new(&mut r, &medium);
          self.decode(&mut d)
       }
    }
@@ -91,12 +94,12 @@ mod tests {
       {
          use ::serialize::OutofOctets;
          let mut f = Foo::default();
-         assert_matches!(f.outof_octets(&buf, "hash"), Ok(13));
+         assert_matches!(f.outof_octets(&buf, ""), Ok(13));
          assert_eq!(f.s.as_str(), "Hatsune Miku");
       }
       {
          use ::serialize::FromOctets;
-         let f = Foo::from_octets(&buf, "hash").unwrap();
+         let f = Foo::from_octets(&buf, "").unwrap();
          assert_eq!(f.s.as_str(), "Hatsune Miku");
       }
    }
@@ -121,11 +124,11 @@ mod tests {
    fn test_to_digest() {
       use ::serialize::ToDigest;
       let f = Bar { s:"Hatsune Miku" };
-      let v = f.to_dhash256_hex_string("hash").unwrap();
+      let v = f.to_dhash256_hex_string("").unwrap();
       assert_eq!("e5d17f17a6ad7a94eec6add232a2fb1c2a848465cc8ad1dc030b6d0caa9294d9", v.as_str());
-      let v = f.to_hash160_hex_string("hash").unwrap();
+      let v = f.to_hash160_hex_string("").unwrap();
       assert_eq!("b7233a798e6ea977644ded49241c2b153a6617b9", v.as_str());
-      let v = f.to_hash160_hex_string_rev("hash").unwrap();
+      let v = f.to_hash160_hex_string_rev("").unwrap();
       assert_eq!("b917663a152b1c2449ed4d6477a96e8e793a23b7", v.as_str());
    }
 }
